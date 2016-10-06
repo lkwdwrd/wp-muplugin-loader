c3da0de543800dc0931360fb5b2d9541330e111d
Luke Woodward <woodward.lucas@gmail.com>
2016-10-05 23:10:12 -0500

    Adding testing framework

---
+++ tests/phpunit/MULoaderPlugin_Tests.php
@@ -0,0 +1,40 @@
+<?php
+namespace TenUp\ElasticPress_io\Core;
+
+/**
+* This is a very basic test case to get things started. You should probably rename this and make
+* it work for your project. You can use all the tools provided by WP Mock and Mockery to create
+* your tests. Coverage is calculated against your includes/ folder, so try to keep all of your
+* functional code self contained in there.
+*
+* References:
+*   - http://phpunit.de/manual/current/en/index.html
+*   - https://github.com/padraic/mockery
+*   - https://github.com/10up/wp_mock
+*/
+
+use WP_Mock;
+use WP_Mock\Tools\TestCase as TestCase;
+
+class MULoaderPlugin_Tests extends TestCase {
+	protected $testFiles = [];
+
+	public function setUp() {
+		if ( ! empty( $this->testFiles ) ) {
+			foreach ( $this->testFiles as $file ) {
+				if ( file_exists( PROJECT . $file ) ) {
+					require_once( PROJECT . $file );
+				}
+			}
+		}
+
+		parent::setUp();
+	}
+
+	/**
+	 * Test setup method.
+	 */
+	public function test_setup() {
+		$this->assertTrue( true );
+	}
+}
